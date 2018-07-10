4e9861c061aa6ab4163e5fbb712ae74f8b6b7bcb
augustus <augustus@gorilladick.daddy.com>
2018-07-10 08:51:09 -0400

    corrected forked code author info

--- web-server/app.js
+++ web-server/app.js
@@ -1,26 +1,30 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var errorhandler = require("errorhandler");
 
-app.configure(function(){
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(app.router);
-	app.set('view engine', 'jade');
-	app.set('views', __dirname + '/public');
-	app.set('view options', {layout: false});
-	app.set('basepath',__dirname + '/public');
-});
+// app.use(express.methodOverride());
+app.use(bodyParser);
+app.set("view engine", "jade");
+app.set("views", __dirname + "/public");
+app.set("view options", { layout: false });
+app.set("basepath", __dirname + "/public");
 
-app.configure('development', function(){
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+var env = process.env.NODE_ENV || "development";
 
-app.configure('production', function(){
-	var oneYear = 31557600000;
-	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-	app.use(express.errorHandler());
-});
+if ("development" == env) {
+  // configure stuff here
+  app.use(express.static(__dirname + "/public"));
+  app.use(errorhandler({ dumpExceptions: true, showStack: true }));
+}
 
-console.log("Web server has started.\nPlease log on http://127.0.0.1:3002/index.html");
+if ("production" == env) {
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + "/public", { maxAge: oneYear }));
+  app.use(errorhandler());
+}
+
+console.log(
+  "Web server has started.\nPlease log on http://127.0.0.1:3002/index.html"
+);
 app.listen(3002);
