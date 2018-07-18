01c69296cf5efc3cc8a9bd945dd2f3f2e4dadd18
augustus <augustus@gorilladick.daddy.com>
2018-07-18 14:54:40 -0400

    starting on the Front End

---
+++ poker-back-end/poker-example.js
@@ -0,0 +1,33 @@
+var poker = require("./lib/node-poker");
+
+var table = new poker.Table(50, 100, 4, 10, 100, 1000);
+
+table.AddPlayer("bob", 1000);
+table.AddPlayer("jane", 1000);
+table.AddPlayer("dylan", 1000);
+table.AddPlayer("john", 1000);
+
+table.StartGame();
+
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+table.players[1].Bet(50);
+table.players[2].Bet(1);
+console.log(table.players[1]);
+console.log(table.players[2]);
+table.players[3].Call();
+table.players[0].Call();
+table.players[1].Call();
+table.players[2].Call();
+table.players[3].Call();
+table.players[0].Call();
+
+console.log(table.game);
+
+//table.initNewRound()
