5b61f396d85e0f3daeb11c2c32e5fa87e8576868
augustus <augustus@gorilladick.daddy.com>
2018-08-13 19:10:41 -0400

    refactored Table model

--- models/Table.js
+++ models/Table.js
@@ -1,50 +1,104 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const db = require("../db");
 
-// Create schema
-const TableSchema = new Schema({
-  smallBlind: {
-    type: Number,
-    default: 1000
-  },
-  bigBlind: {
-    type: Number,
-    default: 2000
-  },
-  minPlayers: {
-    type: Number,
-    default: 2
-  },
-  maxPlayers: {
-    type: Number,
-    default: 5
-  },
-  minBuyIn: {
-    type: Number,
-    default: 100000 // min buy in 100k sats
-  },
-  maxBuyIn: {
-    type: Number,
-    default: 100000000 // max buy in 1 BTC = 100m Sats
-  },
-  players: [
-    {
-      type: Schema.Types.ObjectId, // profiles is an array of profile objects, importing schema
-      ref: "users"
-    }
-  ],
-  dealerPosition: {
-    // represents index in players array of user who is currently dealer
-    type: Number,
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+// @params NA
+// @return table if already created, else false
+// @desc find the first table in the DB. This is temporary until we add ability for multiple tables
+const findTable = async () => {
+  const { rows } = await db.query("SELECT * FROM lnpoker.tables limit 1");
+
+  if (rows.length < 1) {
+    return false;
   }
-});
 
-module.exports = Table = mongoose.model("tables", TableSchema);
+  return rows[0];
+};
+
+// @params NA
+// @return table object without players array
+// @desc create a new table using default params instead of destructuring table arguments
+const createNewTable = async () => {
+  return await db.query("INSERT INTO lnpoker.tables DEFAULT VALUES returning *")
+    .rows[0];
+};
+
+// @params tableID and userID
+// @return table object with players array
+// @desc create a new table using default params instead of destructuring table arguments
+const joinTable = async (tableID, userID) => {
+  // append user id to table | auto join the table you create
+  await db.query(
+    "INSERT INTO lnpoker.user_table(player_id, table_id) VALUES ($1, $2)",
+    [userID, tableID]
+  );
+};
+
+// @params tableID
+// @return array
+// @desc return array of users found on table
+const getPlayersAtTable = async tableID => {
+  const players = await db.query(
+    "SELECT username, dealer, chips, folded, allin, talked, cards FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.table_id = $1",
+    [tableID]
+  );
+
+  if (players.rows.length > 0) {
+    return players.rows;
+  }
+  // else return empty array
+  return [];
+};
+
+// @params tableID and userID
+// @return bool
+// @desc return true if user is found on table, else false
+const isPlayerOnTable = async (userID, tableID) => {
+  const result = await db.query(
+    "SELECT username FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.player_id = $1 and lnpoker.user_table.table_id = $2",
+    [userID, tableID]
+  );
+
+  if (result.rows.length > 0) {
+    return true;
+  }
+
+  return false;
+};
+
+// @desc - join existing table
+// @params - cb is a callback function that takes errors as it's first param, and table state as second. userID takes user id from requesting user
+// returns errors or table data
+const joinTableIfItExists = async (cb, userID) => {
+  let table;
+  try {
+    table = await findTable();
+    // create new table if none found
+    if (!table) {
+      table = await createNewTable();
+
+      await joinTable(table.id, userID);
+
+      table.players = await getPlayersAtTable(table.id);
+
+      return cb(null, table);
+    }
+
+    // return table if player is already on table
+    if (isPlayerOnTable(userID, table.id)) {
+      table.players = await getPlayersAtTable(table.id);
+      return cb(null, table);
+    }
+    // set player object to requesting user's id if above are false
+    // add the user to the table
+    await joinTable(table.id, userID);
+
+    table.players = await getPlayersAtTable(table.id);
+  } catch (error) {
+    return cb(error);
+  }
+  return cb(null, table);
+};
+
+module.exports = { joinTableIfItExists };
 
 // START GAME, TABLE STATE: Table {
 //   smallBlind: 50,
--- routes/api/game.js
+++ routes/api/game.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const Router = require("express-promise-router");
 const router = new Router();
-const db = require("../../db");
+const { joinTableIfItExists } = require("../../models/Table");
 
 // @route   POST api/game
 // @desc    Create a new table if user hasn't already created / joined another table and persist to DB
@@ -9,7 +9,7 @@ const db = require("../../db");
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
+  (req, res) => {
     // callback function that returns error or table object
     const getTableCb = (errors, tableFromCaller = {}) => {
       if (errors) {
@@ -26,77 +26,5 @@ router.post(
     joinTableIfItExists(getTableCb, req.user.id);
   }
 );
-
-// @desc - join existing table
-// @params - cb is a callback function that takes errors as it's first param, and table state as second. userID takes user id from requesting user
-// returns errors or table data
-async function joinTableIfItExists(cb, userID) {
-  let table;
-  try {
-    // find the first table in the DB. This is temporary until we add ability for multiple tables
-    // table = await Table.findOne().populate("players", "name");
-    const { rows } = await db.query("SELECT * FROM lnpoker.tables limit 1");
-    // if none exists then create a new table
-    if (rows.length < 1) {
-      // create a new table
-      // using default params instead of destructuring table arguments
-      table = await db.query(
-        "INSERT INTO lnpoker.tables DEFAULT VALUES returning *"
-      ).rows[0];
-
-      // append user id to table | auto join the table you create
-      await db.query(
-        "INSERT INTO lnpoker.user_table(player_id, table_id) VALUES ($1, $2)",
-        [userID, table.id]
-      );
-      const players = await db.query(
-        "SELECT username, dealer, chips, folded, allin, talked, cards FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.table_id = $1",
-        [table.id]
-      );
-
-      if (players.rows.length > 0) {
-        table.players = players.rows;
-      }
-      return cb(null, table);
-    }
-
-    // get player if he already exists on table
-    table = rows[0];
-    const player = await db.query(
-      "SELECT username FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.player_id = $1 and lnpoker.user_table.table_id = $2",
-      [userID, table.id]
-    );
-
-    // return table if player is already on table
-    if (player.rows.length > 0) {
-      const players = await db.query(
-        "SELECT username, dealer, chips, folded, allin, talked, cards FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.table_id = $1",
-        [table.id]
-      );
-      if (players.rows.length > 0) {
-        table.players = players.rows;
-      }
-      return cb(null, table);
-    }
-
-    // set player object to requesting user's id if above are false
-    // add the user to the table
-    await db.query(
-      "INSERT INTO lnpoker.user_table(player_id, table_id) VALUES ($1, $2)",
-      [userID, table.id]
-    );
-    const players = await db.query(
-      "SELECT username, dealer, chips, folded, allin, talked, cards FROM lnpoker.users INNER join lnpoker.user_table on lnpoker.users.id = lnpoker.user_table.player_id WHERE lnpoker.user_table.table_id = $1",
-      [table.id]
-    );
-
-    if (players.rows.length > 0) {
-      table.players = players.rows;
-    }
-  } catch (error) {
-    return cb(error);
-  }
-  return cb(null, table);
-}
 
 module.exports = router;
