839df417125c49fa80e156ff10963c803055b86a
Anthony E <anthonyebin@gmail.com>
2018-11-23 18:20:06 -0500

    removed dev tools requirement from store

--- poker-front-end/src/store.js
+++ poker-front-end/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -9,10 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialSate,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
